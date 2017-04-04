4647f0a1f9d7dbdc4f5fd8feb4aa126d801dd268
SahilAgarwal321 <sahil.agarwal94@gmail.com>
2017-04-04 13:30:07 +0530

    Add files

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name='csv_creator',
+      version='1.0',
+      description='Creates multiple recursive CSV files',
+      author='Sahil Agarwal',
+      author_email='sahil.agarwal94@gmail.com',
+      keywords='csv csv_maker csv_creator config configuration',
+      # url='',
+      license='MIT',
+      classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Developers',
+      'Topic :: Software Development :: Logging Tools',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 2',
+      'Programming Language :: Python :: 2.6',
+      'Programming Language :: Python :: 2.7',
+      ],
+
+      packages=find_packages(),
+      )
+
